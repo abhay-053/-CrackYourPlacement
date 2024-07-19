b8cecc37aaa472c43f551b88b7b332b5ff2c41e7
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2024-07-19 23:22:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (27.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        for(auto i: s)
+        {
+            if(i=='('||i=='{'||i=='[')
+            {
+                st.push(i);
+            }
+            else 
+            {
+                if((!st.empty() && i== ')' && st.top() == '(')||(!st.empty() &&i== '}' && st.top() == '{')||(!st.empty() &&i== ']' && st.top() == '['))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(st.size()==0)return true;
+        return false;
+    }
+};
