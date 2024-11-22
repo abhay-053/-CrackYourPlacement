c614bfe415bacfa5c092adf8547f76af5a979cb6
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2024-11-22 18:41:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -6,42 +6,35 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    int dr[8] ={-1,-1,-1,0,0,1,1,1};
-    int dc[8] ={-1,0,1,1,-1,-1,0,1};
-    void dfs(vector<vector<char>>& grid, int row, int col, int n, int m)
+    void dfs(vector<vector<char>> &grid, int r, int c, int n, int m)
     {
-        grid[row][col]='0';
+        if(r < 0 || r>=n || c<0 || c>=m || grid[r][c] == '0')return;
         
-        
-        
-        for(int i=0;i<8;i++)
-        {
-            int nr= row+dr[i];
-            int nc= col+dc[i];
-            
-            if(nr>=0 && nr<n && nc >=0 && nc<m && grid[nr][nc] != '0')
-            {
-                dfs(grid,nr,nc,n,m);
-            }
-        }
+        grid[r][c] = '0';
+        dfs(grid,r-1,c-1, n,m);
+        dfs(grid,r-1,c, n,m);
+        dfs(grid,r-1,c+1, n,m);
+        dfs(grid,r,c-1, n,m);
+        dfs(grid,r,c+1, n,m);
+        dfs(grid,r+1,c-1, n,m);
+        dfs(grid,r+1,c, n,m);
+        dfs(grid,r+1,c+1, n,m);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int ans =0;
-        int n=grid.size();
-        int m=grid[0].size();
-        
-        for(int i=0;i<n;i++)
+        // Code here
+        int count = 0;
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<grid[0].size();j++)
             {
-                if(grid[i][j]=='1')
+                if(grid[i][j] == '1')
                 {
-                    ans++;
-                    dfs(grid,i,j,n,m);
+                    dfs(grid, i,j,grid.size(), grid[0].size());
+                    count++;
                 }
             }
         }
-        return ans;
+        return count ;;
     }
 };
 
@@ -61,7 +54,9 @@ int main() {
         Solution obj;
         int ans = obj.numIslands(grid);
         cout << ans << '\n';
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 // } Driver Code Ends
