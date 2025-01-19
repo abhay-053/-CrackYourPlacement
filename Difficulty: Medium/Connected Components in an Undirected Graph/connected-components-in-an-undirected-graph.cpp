0fcffb8f896be4fda869a4bae31f8c926341e561
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2025-01-19 15:10:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
@@ -29,16 +29,6 @@ class Matrix {
 
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, vector<bool>& visited, stack<int>& st, int node) {
-        visited[node] = true;
-        for (auto i : adj[node]) {
-            if (!visited[i]) {
-                dfs(adj, visited, st, i);
-            }
-        }
-        st.push(node);
-    }
-
     void collectComponent(vector<vector<int>>& adj, vector<bool>& visited, int node, vector<int>& component) {
         visited[node] = true;
         component.push_back(node);
@@ -50,42 +40,20 @@ public:
     }
 
     vector<vector<int>> connectedcomponents(int n, vector<vector<int>>& edges) {
-        // Build adjacency list
+
         vector<vector<int>> adj(n);
         for (auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]); // Since it's an undirected graph
+            adj[edge[1]].push_back(edge[0]); 
         }
-
-        stack<int> st;
         vector<bool> visited(n, false);
-
-        // Step 1: Perform DFS to fill stack with nodes based on finish time
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(adj, visited, st, i);
-            }
-        }
-
-        // Step 2: Transpose the graph
-        vector<vector<int>> adj1(n);
-        for (int i = 0; i < n; i++) {
-            for (auto j : adj[i]) {
-                adj1[j].push_back(i);
-            }
-        }
-
-        // Step 3: Perform DFS on the transposed graph
-        fill(visited.begin(), visited.end(), false); // Reset visited
         vector<vector<int>> ans;
 
-        while (!st.empty()) {
-            int node = st.top();
-            st.pop();
+        for(int i=0;i<n;i++){
 
-            if (!visited[node]) {
+            if (!visited[i]) {
                 vector<int> component;
-                collectComponent(adj1, visited, node, component);
+                collectComponent(adj, visited,i, component);
                 sort(component.begin(), component.end());
                 ans.push_back(component);
             }
