8a840fa3d04a06668ba549b1d5541622f6d1e5ca
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-06-13 23:01:18 +0530

    Time: 422 ms (50.42%), Space: 403.2 MB (21.9%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
@@ -103,7 +103,6 @@ public:
             if(u != v){
                 jump += 2;
             }
-            cout<<k<<endl;
             int totalWays = 0;
 
             totalWays = (totalWays +  binaryExponentiation(2ll, 1ll*(jump - 1)) ) % mod;
