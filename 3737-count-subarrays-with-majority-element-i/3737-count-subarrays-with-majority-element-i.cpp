642d6f5989782cf35af9a68b595a1c72f3633242
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-06-25 16:49:09 +0530

    Time: 55 ms (26.8%), Space: 54 MB (10.24%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -0,0 +1,34 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+class Solution {
+public:
+    
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> prefix(n+1, 0);
+        int j = 1;
+        for(auto &i: nums){
+            if(i == target){
+                prefix[j] = 1;
+            } else {
+                prefix[j] = -1;
+            }
+            j++;
+        }
+
+        for(int i = 1; i<= n; i++){
+            prefix[i] += prefix[i-1];
+        }
+
+        tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> st;
+        int ans = 0;
+        for(int i = 0; i<= n; i++){
+            ans += st.order_of_key({prefix[i], -1});
+            st.insert({prefix[i], i });
+        }
+
+        return ans;
+    }
+};
