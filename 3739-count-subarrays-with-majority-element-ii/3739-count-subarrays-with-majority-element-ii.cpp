bdffd34b47369e2afe8f1e1f3e0a481e8fbd1211
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-06-26 16:50:51 +0530

    Time: 74 ms (55.63%), Space: 114.8 MB (49.01%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,32 +1,29 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
 
-        vector<int> prefix(n+1, 0);
-        int j = 1;
-        for(auto &i: nums){
-            if(i == target){
-                prefix[j] = 1;
-            } else {
-                prefix[j] = -1;
-            }
-            j++;
-        }
-
-        for(int i = 1; i<= n; i++){
-            prefix[i] += prefix[i-1];
-        }
-
-        tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> st;
+        int curSum = 0; 
+        int validLeftPoints = 0;
         long long ans = 0;
-        for(int i = 0; i<= n; i++){
-            ans += st.order_of_key({prefix[i], -1});
-            st.insert({prefix[i], i });
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        for(int i = 0; i< n; i++){
+
+            if(nums[i] == target){
+                validLeftPoints += mp[curSum];
+                curSum += 1;
+            } else {
+                curSum -= 1;
+                validLeftPoints -= mp[curSum];
+            }
+
+            mp[curSum] += 1;
+            ans += validLeftPoints;
+
         }
+
+        
 
         return ans;
     }
