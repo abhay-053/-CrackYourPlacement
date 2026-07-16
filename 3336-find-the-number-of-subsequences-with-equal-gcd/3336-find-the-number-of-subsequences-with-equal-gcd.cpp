95d432d26747c97f7de0248d9c117f942b8ad2ff
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-07-16 17:13:54 +0530

    Time: 1562 ms (8.68%), Space: 90 MB (59.74%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -27,28 +27,27 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         n = nums.size();
         // memset(dp, -1, sizeof(dp));
-
         int maxVal = *max_element(begin(nums), end(nums));
-        int dp[n+1][maxVal +1][maxVal + 1];
-        for(int first = 0; first <= maxVal ; first ++){
-            for(int second = 0; second <= maxVal; second ++){
-                if(first != 0 && second != 0 && first == second){
-                    dp[n][first][second] = 1;
-                } else {
-                    dp[n][first][second] = 0;
-                }
-            }
-        }
-
-        for(int i = n-1; i >= 0; i--){
+        int dp[n+1][maxVal + 1][maxVal + 1];
+        for(int i = n; i >= 0; i--){
             for(int first = maxVal; first >= 0; first --){
                 for(int second = maxVal; second >= 0; second --){
 
-                    int skip = dp[i+1][first][second];
-                    int take1 = dp[i+1][__gcd(first, nums[i])][second];
-                    int take2 = dp[i+1][first][__gcd(second, nums[i])];
+                    if(i == n){
+                        if(first != 0 && second != 0 && first == second){
+                            dp[i][first][second] = 1;
+                        } else {
+                            dp[i][first][second] = 0;
+                        }
+                    }
+                    else {
+                        int skip = dp[i+1][first][second];
+                        int take1 = dp[i+1][__gcd(first, nums[i])][second];
+                        int take2 = dp[i+1][first][__gcd(second, nums[i])];
 
-                    dp[i][first][second] = ((skip + take1) % MOD + take2) % MOD;
+
+                        dp[i][first][second] = ((skip + take1) % MOD + take2) % MOD;
+                    }
                 }
             }
         }
