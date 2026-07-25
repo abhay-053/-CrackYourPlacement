0cce9a118d0526a8fa83a0fcce9cec507caf3a88
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-07-25 23:30:09 +0530

    Time: 123 ms (5.01%), Space: 166.3 MB (5.02%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int n = s.size();
+
+        if(numRows == 1){
+            return s;
+        }
+        vector<vector<char>> arr(numRows, vector<char>(1000, ' '));
+
+        int i = 0, j = 0;
+        int dir = 0;
+        int k = 0;
+        while(k < n){
+            arr[i][j] = s[k++];
+
+            if(i == 0){
+                dir = 0;
+            } else if(i == numRows - 1){
+                dir = 1;
+            }
+
+            if(dir == 0){
+                i++;
+            } else {
+                i--;
+                j++;
+            }
+        }
+        string str;
+        for(auto i: arr){
+            for(auto j: i){
+                if(j != ' '){
+                    str += j;
+                }
+            }
+        }
+        return str;
+    }
+};
