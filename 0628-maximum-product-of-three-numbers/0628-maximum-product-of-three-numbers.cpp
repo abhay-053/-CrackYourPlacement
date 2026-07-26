f74c2d85783e9a2c981be74d284dfd4fbb70c668
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-07-26 09:04:45 +0530

    Time: 0 ms (100%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
-    int maximumProduct(vector<int>& nums)   
-    {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        return max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
+    int maximumProduct(vector<int>& nums) {
+        int max_1 = -1001, max_2 = -1001, max_3 = -1001;
+        int max_11 = 1001, max_22 = 1001;
+        for(int num: nums){
+            if(num > max_1){
+                max_3 = max_2;
+                max_2 = max_1;
+                max_1 = num;
+            } else if( num > max_2){
+                max_3 = max_2;
+                max_2 = num;
+            } else if(num > max_3){
+                max_3 = num;
+            }
+
+            if( num < max_11){
+                max_22 = max_11;
+                max_11 = num;
+            } else if( num < max_22){
+                max_22 = num;
+            }
+        }
+        // cout<<max_1<<" "<<max_2<<" "<<max_11<<" "<<max_22<<" "<<max_3<<endl;
+        
+        int ans = max_1 * max_2 * max_3 ;
+
+        ans = max(max_11 * max_22 * max_1 , ans);
+        return ans;
     }
 };
