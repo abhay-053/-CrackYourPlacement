d3e588157d61cd83d664f9471f7dd9ab386f4868
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2026-08-20 00:38:42 +0530

    Time: 45 ms (52.36%), Space: 69.1 MB (44.93%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, vector<int>> mp;
+
+        for (auto &x : reservedSeats) {
+            mp[x[0]].push_back(x[1]);
+        }
+
+        int ans = (n - mp.size()) * 2;
+
+        for (auto &[row, seats] : mp) {
+            bool left = true;
+            bool middle = true;
+            bool right = true;
+
+            for (int seat : seats) {
+                if (seat >= 2 && seat <= 5)
+                    left = false;
+
+                if (seat >= 4 && seat <= 7)
+                    middle = false;
+
+                if (seat >= 6 && seat <= 9)
+                    right = false;
+            }
+
+            if (left && right)
+                ans += 2;
+            else if (left || middle || right)
+                ans += 1;
+        }
+
+        return ans;
+    }
+};
